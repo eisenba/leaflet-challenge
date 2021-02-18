f2c82209b85de1b85ed664b6f3abb815ae06ffe7
Matt Eisenbart <matt.eisenbart@gmail.com>
2021-02-18 06:06:53 -0600

    Adds markerColor

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -18,6 +18,9 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var quakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
 var max_depth = 0
 var min_depth = 0
+var depthCount = 0
+var depthSum = 0
+var avg_depth = 0
 // Start d3
 d3.json(quakeData, function(data){
   // find min and max depth
@@ -28,17 +31,26 @@ d3.json(quakeData, function(data){
       min_depth = data.features[i].geometry.coordinates[2]
     }
   }
+  // create array of depths to calc average
+  for (let i= 0; i< data.features.length; i++) {
+    depthCount++;
+    depthSum += data.features[i].geometry.coordinates[2]
+  }
+  // Calc avg depth
+  avg_depth = depthSum/depthCount
   // Define marker size function
   function markerSize(magnitude) {
     return magnitude*5
   }
 
   // Define marker color function
-  function markerColor(depth) {
-    
-  }
+  markerColor = d3.scaleLinear()
+  .domain([min_depth, avg_depth, max_depth])
+  .range(["chartreuse", "gold", "red"])
+  
 console.log(max_depth)
 console.log(min_depth)
+console.log(avg_depth)
 });
  
 
